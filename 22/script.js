1af60450378b173d76318f9a992acf44272778e3
carlos henrique <henriquebahia200@gmail.com>
2023-12-15 16:29:46 -0300

    Add files via upload

---
+++ 22/script.js
@@ -0,0 +1,31 @@
+
+       function MinhaFuncao1(arg) {
+
+       	var MinhaFuncao1 = Math.sqrt(arg);
+
+       	var msg = ('A raiz de 25 é : ') ;
+
+       	alert(msg+MinhaFuncao1);
+
+       }
+         function Raiz2(arg) {
+
+       	var raiz = Math.sqrt(arg);
+
+       	var msg = ('A raiz de 36 é : ' );
+
+       	alert(msg+raiz);
+
+       }
+         function Raiz3(arg) {
+
+       	var raiz = Math.sqrt(arg);
+
+       	var msg = ('A raiz de 49 é : ') ;
+
+       	alert(msg+raiz);
+
+       }
+
+     
+
---
+++ 22/soma.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+function  minha_funcao(num1, num2) {
+	var soma = num1 + num2;
+	alert(' A soma é igual é : '+soma);
+	// body...
+}
+
+
+</script>
